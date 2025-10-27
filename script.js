f12feeddeebd7c6123751575302d4f0de6f62233
tahaBilal789 <tahabilal482@gmail.com>
2025-10-27 06:26:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var Typed = new Typed('#element', {
+    strings: ['  Full stack developer.', '  Frot end developer.','Back end developer.'],
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 50,
+}); 
